fcd096db031f374a4ebd2c32ed42a931fa8dcaaa
Chris443C <chris.ince@443cyber.com>
2025-03-03 15:24:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify, send_file
+from flask_socketio import SocketIO
 import os
 import subprocess
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app, async_mode='eventlet')
 
 REPORTS_DIR = 'reports'
 if not os.path.exists(REPORTS_DIR):
@@ -70,5 +73,13 @@ def bandit_scan():
 def download(filename):
     return send_file(os.path.join(REPORTS_DIR, filename), as_attachment=True)
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
